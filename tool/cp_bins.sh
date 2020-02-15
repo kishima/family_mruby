c122350580eb45855585e7ef7961e17eb3dab684
kishima <kishima7@gmail.com>
2020-02-15 22:03:59 +0900

    add missing spiffs_image to v0.6.0

--- tool/cp_bins.sh
+++ tool/cp_bins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./mkspiffs -c ./spiffs_data -b 4096 -p 256 -s 0x1F0000 spiffs_image
+./mkspiffs -c ./spiffs_data -b 4096 -p 256 -s 0x1F0000 spiffs_image.bin
 
 cp ../build/bootloader/bootloader.bin ../release_bin/
 cp ../build/family_mruby.bin ../release_bin/
 cp ../build/partitions.bin ../release_bin/
-cp ./spiffs_image ../release_bin/
+cp ./spiffs_image.bin ../release_bin/
 
 cd ../release_bin
 zip family_mruby_bin.zip ./*.bin
